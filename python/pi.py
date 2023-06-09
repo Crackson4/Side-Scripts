fc3ee71a5faaa913638d570b8321dff076c0f790
Crackson4 <zachariahhart18@gmail.com>
2023-06-09 19:48:15 +1000

    Used decimal library to increase decimal places of output

--- python/pi.py
+++ python/pi.py
@@ -1,19 +1,16 @@
-pi = float()
-
-limit = 76651
+from decimal import Decimal
 
 
 def main():
-    global pi
-    pi = 3
+    pi = Decimal(3)
     i = 1
-    while i < limit:
+    while True:
         sign = -1 if i % 2 == 0 else 1
         two_i = 2 * i
-        pi += sign * 4 / (two_i * (two_i + 1) * (two_i + 2))
+        pi += Decimal(sign * 4 / (two_i * (two_i + 1) * (two_i + 2)))
         i += 1
-    print(pi)
-    print(len(str(pi)) - 2)
+        if i % 100000 == 0:
+            print(pi)
 
 
 if __name__ == "__main__":
